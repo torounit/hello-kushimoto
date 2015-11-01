59bc8d94b2048f4ae1f4f787832ef5f653e41e99
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2015-11-01 13:40:51 +0900

    add hello dolly

--- hello-kushimoto.php
+++ hello-kushimoto.php
@@ -54,8 +54,43 @@ interface Megumi {
 Class Miyasan implements Megumi {
 
 	public function say() {
-		return "hello";
+
+		$words = explode( "\n", $this->getWords() );
+		return wptexturize( $words[ mt_rand( 0, count( $words ) - 1 ) ] );
 	}
+
+	public function getWords() {
+		return  "Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+So, take her wrap, fellas
+Find her an empty lap, fellas
+Dolly'll never go away again
+Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+We feel the room swayin'
+While the band's playin'
+One of your old favourite songs from way back when
+Golly, gee, fellas
+Find her a vacant knee, fellas
+Dolly'll never go away
+Dolly'll never go away
+Dolly'll never go away again";
+	}
+
+
 
 }
 
