9cc276b7d59ccf596cd2e66e504b88aca9657fe4
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2015-11-03 16:35:45 +0900

    fix naming. add test

---
+++ tests/test-miyasan.php
@@ -0,0 +1,18 @@
+<?php
+
+class Test_Miyasan extends WP_UnitTestCase {
+
+	public function test_getwords() {
+		$miyasan = new Miyasan();
+		$words = $miyasan->getWords();
+		$this->assertTrue( is_array( $words ) );
+
+	}
+
+	public function test_say() {
+		$miyasan = new Miyasan();
+		$miyasan->say();
+		$this->assertTrue( is_string( $miyasan->say() ) );
+
+	}
+}
