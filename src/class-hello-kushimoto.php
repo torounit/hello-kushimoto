37da444116d8776f3807ee10566d8a201b69a8e7
plastikdreams <plastiktraume@gmail.com>
2015-11-03 21:04:58 +0900

    Update class-hello-kushimoto.php
    change class name of CSS, add some lines to display his picture (from github).

--- src/class-hello-kushimoto.php
+++ src/class-hello-kushimoto.php
@@ -36,7 +36,7 @@ class Hello_Kushimoto {
 	 */
 	public function render() {
 		$chosen = $this->speaker->talk_message();
-		echo "<p class='hello-kusimoto'>$chosen</p>";
+		echo "<p class='hello-kushimoto'>$chosen</p>";
 	}
 
 	/**
@@ -46,12 +46,18 @@ class Hello_Kushimoto {
 
 		$x = is_rtl() ? 'left' : 'right';
 		$style = "
-        .hello-kusimoto {
+        .hello-kushimoto {
             float: $x;
             padding-$x: 15px;
             padding-top: 5px;
+            padding-right: 70px;
             margin: 0;
             font-size: 11px;
+            background: #fff url("https://avatars0.githubusercontent.com/u/309946?v=3&s=50") no-repeat;
+            color: #444;
+            border: 1px solid #E5E5E5;
+            border-radius: 1px;
+            box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.04);
         }
         ";
 		wp_add_inline_style( 'wp-admin', $style );
