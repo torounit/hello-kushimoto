c571aff3c1c671e47592d104d277041a0b5a9344
miya0001 <miya@wpist.me>
2015-11-05 23:10:01 +0900

    some fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,34 +7,27 @@ if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
 	exit
 fi
 
-if [[ ! $TRAVIS_TAG ]]; then
-	echo "Not deploying empty tag."
-	exit
-fi
-
 if [[ ! $WP_PULUGIN_DEPLOY ]]; then
 	echo "Not deploying."
 	exit
 fi
 
-if [[ ! $SVN_REPO || ! $GH_REF ]]; then
-	echo "Please set \$GH_REF and \$PLUGIN_REPO in .travis.yml."
-	exit
-fi
+echo "Startng deploy..."
 
 mkdir build
 
 cd build
-svn co $SVN_REPO
-git clone $GH_REF $(basename $SVN_REPO)/git
+svn co -q $SVN_REPO
+git clone -q $GH_REF $(basename $SVN_REPO)/git
 
 cd $(basename $SVN_REPO)
 SVN_ROOT_DIR=$(pwd)
 
-rsync -avz $SVN_ROOT_DIR/git/ $SVN_ROOT_DIR/trunk/
+rsync --checksum -a $SVN_ROOT_DIR/git/ $SVN_ROOT_DIR/trunk/
 rm -fr $SVN_ROOT_DIR/git
 
 cd $SVN_ROOT_DIR/trunk
+echo "Startng bin/build.sh."
 bash bin/build.sh
 cd $SVN_ROOT_DIR
 
@@ -57,14 +50,19 @@ package.json
 phpunit.xml
 tests" > .svnignore
 
-svn propset -R svn:ignore -F .svnignore .
+svn propset -q -R svn:ignore -F .svnignore .
 
 svn propset svn:ignore -F .svnignore trunk/
-svn st | grep '^!' | sed -e 's/\![ ]*/svn del /g' | sh
-svn st | grep '^?' | sed -e 's/\?[ ]*/svn add /g' | sh
+svn st | grep '^!' | sed -e 's/\![ ]*/svn del -q /g' | sh
+svn st | grep '^?' | sed -e 's/\?[ ]*/svn add -q /g' | sh
 
-svn cp trunk tags/$TRAVIS_TAG
+echo "Check statuses before commit."
+svn st
 
-if [[ $SVN_USER && $SVN_PASS ]]; then
-	svn commit -q -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+if [[ $TRAVIS_TAG && $SVN_USER && $SVN_PASS ]]; then
+	echo "Commit to $SVN_REPO."
+	svn cp -q trunk tags/$TRAVIS_TAG
+	svn commit -m "commit version $TRAVIS_TAG" --username $SVN_USER --password $SVN_PASS --non-interactive 2>/dev/null
+else
+	echo "Nothing to commit."
 fi
