5ac3dc6f104d8d76f7a8c6787beae8db7accaaa1
Hiroshi Urabe <mail@torounit.com>
2015-11-06 20:22:01 +0900

    add sampletest

--- tests/test-hello-kushimoto-dashboard-widget.php
+++ tests/test-hello-kushimoto-dashboard-widget.php
@@ -12,4 +12,8 @@ class Test_Hello_Kushimoto_Dashboard_Widget extends WP_UnitTestCase {
 //		global $wp_meta_boxes;
 //		//do_action( 'wp_dashboard_setup' );
 //	}
+
+	public function test_sample() {
+		$this->assertTrue( true );
+	}
 }
