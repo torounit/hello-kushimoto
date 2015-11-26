03c98f6a0c686c923007e1b9e108b0c225561793
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2015-11-27 01:43:23 +0900

    reformat code

--- hello-kushimoto.php
+++ hello-kushimoto.php
@@ -14,18 +14,19 @@ Domain Path: /languages
 
 /**
  * Autoloader
+ *
  * @param string $class_name
  */
 function hello_kushimoto_class_loader( $class_name ) {
-	$dir = dirname( __FILE__ );
-	$file_name = 'class-'.strtolower( str_replace('_', '-', $class_name) ).'.php';
-	$file_path = $dir . '/src/'. $file_name;
+	$dir       = dirname( __FILE__ );
+	$file_name = 'class-' . strtolower( str_replace( '_', '-', $class_name ) ) . '.php';
+	$file_path = $dir . '/src/' . $file_name;
 	if ( is_readable( $file_path ) ) {
 		include $file_path;
 	}
 }
-spl_autoload_register( 'hello_kushimoto_class_loader' );
 
+spl_autoload_register( 'hello_kushimoto_class_loader' );
 
 
 /**
