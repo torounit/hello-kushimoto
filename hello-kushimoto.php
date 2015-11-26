19fe5def97203a0b13c1dd6d87dfa015975d837e
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2015-11-27 01:41:11 +0900

    use autoload.

--- hello-kushimoto.php
+++ hello-kushimoto.php
@@ -12,10 +12,20 @@ Domain Path: /languages
 */
 
 
-require "src/class-hello-kushimoto.php";
-require "src/class-hello-kushimoto-speaker.php";
-require "src/class-hello-kushimoto-random-speaker.php";
-require "src/class-miyasan.php";
+/**
+ * Autoloader
+ * @param string $class_name
+ */
+function hello_kushimoto_class_loader( $class_name ) {
+	$dir = dirname( __FILE__ );
+	$file_name = 'class-'.strtolower( str_replace('_', '-', $class_name) ).'.php';
+	$file_path = $dir . '/src/'. $file_name;
+	if ( is_readable( $file_path ) ) {
+		include $file_path;
+	}
+}
+spl_autoload_register( 'hello_kushimoto_class_loader' );
+
 
 
 /**
