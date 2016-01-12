feaadad33d56cc841b2c2c08c69ca550ed6995bb
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-01-12 18:18:35 +0900

    add thru Wapuu

---
+++ src/speaker/class-taru-wapuu.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Class Taruwapuu
+ */
+class Taru_Wapuu extends Hello_Kushimoto_Random_Speaker {
+
+	/**
+	 * @var string NickName
+	 */
+	protected $name = 'Taru Wapuu';
+
+
+	/**
+	 * @param int $size
+	 *
+	 * @return string
+	 */
+	public function get_avatar( $size = 50 ) {
+		$src = 'https://wordbash.github.io/kyoto-vol1/assets/dest/images/wapuu.svg';
+		return '<img alt="" src="' . $src . '" class="avatar avatar-50" height="' . esc_attr( $size ) . '" width="' . esc_attr( $size ) . '">
+';
+
+	}
+
+	/**
+	 *
+	 * @return String[]
+	 */
+	public function get_words() {
+		return array(
+			__( 'Cheers!', 'hello-kushimoto' ),
+			__( 'カンパーイ!', 'hello-kushimoto' ),
+			__( 'おさけがのみたーい', 'hello-kushimoto' ),
+			__( 'びーるがたりなーい', 'hello-kushimoto' ),
+			__( 'I want Beer!!!', 'hello-kushimoto' ),
+			__( 'びーるがたりなーい', 'hello-kushimoto' ),
+		);
+	}
+
+
+}
