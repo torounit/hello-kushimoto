9835c20adca6673d12237bb74b19d26417b2dedb
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-01-13 16:36:31 +0900

    add once

--- src/class-hello-kushimoto-class-loader.php
+++ src/class-hello-kushimoto-class-loader.php
@@ -28,7 +28,7 @@ class Hello_Kushimoto_Class_Loader {
 		$file_name = 'class-' . strtolower( str_replace( '_', '-', $class_name ) ) . '.php';
 		$file_path = $dir . '/' . $file_name;
 		if ( is_readable( $file_path ) ) {
-			include $file_path;
+			include_once $file_path;
 		}
 	}
 }
