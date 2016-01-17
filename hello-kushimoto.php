a3d8b6f014519bb76150c9d0c44835375088c7e8
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-01-18 03:47:08 +0900

    error fix

--- hello-kushimoto.php
+++ hello-kushimoto.php
@@ -24,10 +24,11 @@ $hello_kushimoto_data = get_file_data( __FILE__, array(
 	'DomainPath' => 'Domain Path',
 	'Network' => 'Network',
 ) );
-define( 'HELLO_KUSHIMOTO_VERSION', $hello_kushimoto_data['Version'] );
-define( 'HELLO_KUSHIMOTO_DOMAIN_PATH',  plugin_basename( HELLO_KUSHIMOTO_DIR ). $hello_kushimoto_data['DomainPath'] );
 define( 'HELLO_KUSHIMOTO_FILE', __FILE__ );
 define( 'HELLO_KUSHIMOTO_DIR', dirname( __FILE__ ) );
+define( 'HELLO_KUSHIMOTO_VERSION', $hello_kushimoto_data['Version'] );
+define( 'HELLO_KUSHIMOTO_DOMAIN_PATH',  plugin_basename( HELLO_KUSHIMOTO_DIR ). $hello_kushimoto_data['DomainPath'] );
+
 
 new Hello_Kushimoto_Class_Loader( HELLO_KUSHIMOTO_DIR . '/src' );
 new Hello_Kushimoto_Class_Loader( HELLO_KUSHIMOTO_DIR . '/src/speaker/abstract' );
