7c0947751e835392c8edede3c78f39b2bf0abd8a
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-12-10 15:25:36 +0900

    uninstall hooks register on activate only.

--- src/class-hello-kushimoto-option-manager.php
+++ src/class-hello-kushimoto-option-manager.php
@@ -13,9 +13,19 @@ class Hello_Kushimoto_Option_Manager {
 	 * Hello_Kushimoto_Option constructor.
 	 */
 	public function __construct() {
+		register_activation_hook( HELLO_KUSHIMOTO_FILE, array( __CLASS__, 'register_uninstall_hook' ) );
+	}
+
+	/**
+	 * Register uninstall hooks. fire on activate this plugin.
+	 */
+	public static function register_uninstall_hook() {
 		register_uninstall_hook( HELLO_KUSHIMOTO_FILE, array( __CLASS__, 'uninstall' ) );
 	}
 
+	/**
+	 * Action for uninstall.
+	 */
 	public static function uninstall() {
 		delete_option( 'hello_kushimoto_speaker' );
 	}
